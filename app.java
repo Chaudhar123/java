eb65cfc5c4ada41c9b6767ce3d1d93ff4abb8491
Chaudhar123 <nitinchaudhary0652@gmail.com>
2025-04-22 23:22:14 +0530

    java code

---
+++ app.java
@@ -0,0 +1,34 @@
+// // import java.util.*;
+
+// public class app{
+//     public static void main(String[] args){
+
+//         // Scanner sc=new Scanner(System.in);
+
+
+//         System.out.println("enter any number : ");
+//         // int n=sc.nextInt();
+
+//         for(int i=1;i<=10;i++){
+//             // System.out.println(n*i);
+//         }
+      
+// // int num=258157;
+// // int res=0;
+
+// // while(num>0){
+// //     int rem=num%10;
+// //     num=num/10;
+// //     res=res+rem;
+ 
+
+// // }
+// // if(res%2==0){
+// //     System.out.println("even number : "+res);
+// // }else{
+// //     System.out.println("odd number : "+res);
+// // }
+// // System.out.println("sum of digits : "+res);
+
+
+//     }}
